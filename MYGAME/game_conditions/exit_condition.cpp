23c19c25d70c49d67d00eb60b21b8bbf3a259d0e
Darya <90909098+snusoedochka@users.noreply.github.com>
2022-10-19 02:58:29 +0300

    lab2
    lab2

---
+++ MYGAME/game_conditions/exit_condition.cpp
@@ -0,0 +1,13 @@
+#include "exit_condition.h"
+
+#include "../cells/cell_exit.h"
+
+bool exit_condition::isMet()
+{
+    return plr
+    && plr->hasParentCell()
+    && (dynamic_cast<cell_exit*>(&plr->getParentCell()));
+
+}
+
+void exit_condition::setPlayer(const player* plr) { this->plr = plr; }
